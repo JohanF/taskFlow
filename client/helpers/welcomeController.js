3855b5a0b3096385b321f45c2946273dbfd9fdf6
Adamsc <sco3.1415@gmail.com>
2015-09-23 15:38:39 +0200

    reactive login

---
+++ client/helpers/welcomeController.js
@@ -0,0 +1,15 @@
+Template.welcomeScreen.rendered = function() {
+    this.autorun(function(){
+        if(Meteor.userId()){
+            //showloggedin
+            //replace loggedInText with full template with projectview
+            $("#loggedInText").show();
+            $("#notLoggedInText").hide();
+        } else {
+            //hidelogin
+            $("#loggedInText").hide();
+            $("#notLoggedInText").show();
+        }
+       
+    });
+}
