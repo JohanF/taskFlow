82e315f3c99e42e15bd3a8efaae53f012a96d35c
JohanF <johan.fredriksson@outlook.com>
2015-10-03 16:16:43 +0200

    Tasklist properly shows a sorted subset

--- client/templates/application/tasks/tasks_list.js
+++ client/templates/application/tasks/tasks_list.js
@@ -1,8 +1,14 @@
 Template.tasksList.helpers({ 
  tasks: function () {
-      var x = Projects.findOne({"projectTasks.assignedUsers":Meteor.userId()}, {projectTasks : 1}).projectTasks;
+      var x = Projects.findOne({"projectTasks.assignedUsers":Meteor.userId()}, {projectTasks : 1});
 
-      return x.find({assignedUsers:Meteor.userId()});
+      if(!x) return [];
+
+      var tempArray = x.projectTasks || [];
+
+      return _.sortBy(_.filter(tempArray, function(temp) {
+      	return temp.assignedUsers == Meteor.userId();        // Filters and keeps tasks that belongs to the user
+      }), function(sortTemp){ return sortTemp.priority; });  // then sorts them by priority.
 
       // testing @TODO add active project
 
