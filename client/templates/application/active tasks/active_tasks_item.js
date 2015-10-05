7e955553929cbe888d10f2177c3af81c300d0d96
JohanF <johan.fredriksson@outlook.com>
2015-10-05 17:07:57 +0200

    Working on task list

--- client/templates/application/active tasks/active_tasks_item.js
+++ client/templates/application/active tasks/active_tasks_item.js
@@ -1,5 +1,5 @@
 Template.activeTaskItem.helpers({ 
- userName: function () {
+ projectUsers: function () {
  		return Meteor.users.find({_id:Meteor.userId()}).fetch()[0].username;
  		// testing @TODO implement the correct user fetch logic
      } 
--- client/templates/application/active tasks/active_tasks_list.js
+++ client/templates/application/active tasks/active_tasks_list.js
@@ -5,9 +5,22 @@ Template.activeTasksList.helpers({
       // @TODO Sort by createDate
       //var tempArray = x.projectTasks || [];
 
-      return [_.min(Tasks.find({assignedUsers:Meteor.userId(), project:"B3MowNggmDxdo5ugL"}).fetch(), function(temp) { return temp.priority;})];        
+      var projectTasks = Tasks.find({project:"B3MowNggmDxdo5ugL"}).fetch();
+      var userProjectTasks = _.groupBy(projectTasks, 'assignedUsers');
+
+      // @TODO Implement!
+      //return _.min(userProjectTasks, function(temp) { return temp.priority;});        
       // testing @TODO priority has to be changed to be contexual per user
       // testing @TODO add active project
 
+
+      // Test 1::
+      // _.map(_.groupBy(Tasks.find({project:"B3MowNggmDxdo5ugL"}).fetch(), 'assignedUsers'), function(obj){_.min(obj,function(o){return o.priority})})
+      
+      // Test 2::
+      //_.each(_.values(_.groupBy(Tasks.find({project:"B3MowNggmDxdo5ugL"}).fetch(), 'assignedUsers')), alert)
+
+      // _.groupBy([], )
+      //_.groupBy(['one', 'two', 'three'], 'length')  => {3: ["one", "two"], 5: ["three"]}
     } 
 });
