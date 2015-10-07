7430b1472610600b700d2c3897b6e38113d07295
Adamsc <sco3.1415@gmail.com>
2015-10-07 15:56:44 +0200

    added a lot more page functionality. eg chat and profile

---
+++ client/helpers/overlayController.js
@@ -0,0 +1,17 @@
+loadProfile = function() {
+    Router.go("profileview", {
+        _userId: Meteor.userId()
+    });
+}
+goHome = function() {
+    Router.go("/");
+}
+
+loadChat = function() {
+    Router.go("chatview", {
+        _userId: Meteor.userId()
+    });
+}
+killWorld = function(){
+	Router.go("/killworld");
+}
