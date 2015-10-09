e0f11229645dc58497941c28327cf665575c955b
JohanF <johan.fredriksson@outlook.com>
2015-10-09 17:14:29 +0200

    Alligned user login correctly, fixed the correct task activity shell

--- client/component/taskModal.js
+++ client/component/taskModal.js
@@ -2,10 +2,11 @@ Template.taskModal.events({
     'click #closeTaskModal': function () {
        //close modal
         $("#taskModal").hide();
-    },
-    'click #createGameName': function () {
-        var pName = $('#newProjectName').val();
-        var pDesc = $('#newProjectDescription').val();
-        console.log("pName: "+ pName+ " pDesc: "+ pDesc);
-        $("#taskModal").hide();    },
+    }
+});
+
+Template.taskModal.helpers({
+ taskActiviy: function () {
+      return TaskActivities.find({});
+    }
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,42 +1,42 @@
 if (Tasks.find().count() === 0) {
   Tasks.insert({
-    title: 'Do laundry',    
-	description: 'Dirty clothes are bad, fix.',    
+    title: 'Do laundry',
+	description: 'Dirty clothes are bad, fix.',
 	status: '50%',
 	priority: 2,
 	        assignedUsers : ["J7DZRBWTmGoW3StH6,dnTuiWp6D8DoXMTBk"],
 	        project: "B3MowNggmDxdo5ugL"
   });
-  
+
   Tasks.insert({
-    title: 'Eat food',    
-	description: 'Eat food or die',    
-	status: 'Cooked', 
+    title: 'Eat food',
+	description: 'Eat food or die',
+	status: 'Cooked',
 	priority: 1,
 	        assignedUsers : ["J7DZRBWTmGoW3StH6,dnTuiWp6D8DoXMTBk"],
 	        project: "B3MowNggmDxdo5ugL"
   });
-  
+
   Tasks.insert({
-    title: 'Go to bed',    
-	description: 'It is nice',    
-	status: 'Insomnia ETA: 37 minutes',  
+    title: 'Go to bed',
+	description: 'It is nice',
+	status: 'Insomnia ETA: 37 minutes',
 	priority: 3,
 	        assignedUsers : ["J7DZRBWTmGoW3StH6,dnTuiWp6D8DoXMTBk"],
 	        project: "B3MowNggmDxdo5ugL"
   });
   Tasks.insert({
-    title: 'Wake up',    
-	description: 'It is also nice',    
-	status: 'Insomnia ETA: 37 minutes',  
+    title: 'Wake up',
+	description: 'It is also nice',
+	status: 'Insomnia ETA: 37 minutes',
 	priority: 4,
 	        assignedUsers : ["J7DZRBWTmGoW3StH6,dnTuiWp6D8DoXMTBk"],
 	        project: "B3MowNggmDxdo5ugL"
   });
   Tasks.insert({
-    title: 'Go to bed',    
-	description: 'It is nice',    
-	status: 'Insomnia ETA: 37 minutes',  
+    title: 'Go to bed',
+	description: 'It is nice',
+	status: 'Insomnia ETA: 37 minutes',
 	priority: 3,
 	        assignedUsers : [""],
 	        project: ""
@@ -44,23 +44,42 @@ if (Tasks.find().count() === 0) {
 }
 
 if (Projects.find().count() === 0) {
-  
+
   Projects.insert({
-    title: 'My first project',    
-	description: 'testing purposes...',    
+    title: 'My first project',
+	description: 'testing purposes...',
 	members: ["wnc84uM4xND6aX3pB"],
 	creator: "Hacker Man"
   });
   Projects.insert({
-    title: 'The second project',    
-	description: 'testing purposes...',    
+    title: 'The second project',
+	description: 'testing purposes...',
 	members: ["dnTuiWp6D8DoXMTBk"],
 	creator: "Hacker Man"
   });
   Projects.insert({
-    title: 'Ice cream',    
-	description: 'testing purposes...',    
+    title: 'Ice cream',
+	description: 'testing purposes...',
 	members: ["dnTuiWp6D8DoXMTBk"],
 	creator: "dnTuiWp6D8DoXMTBk"
   });
+}
+
+if (TaskActivities.find().count() === 0) {
+    TaskActivities.insert({
+      description:'Task created'
+    });
+    TaskActivities.insert({
+      description:'Working on task'
+    });
+    TaskActivities.insert({
+      description:'Half way there..'
+    });
+    TaskActivities.insert({
+      description:'Almost done..'
+    });
+    TaskActivities.insert({
+      description:'Completed!'
+    });
+
 }
