c6e35d1e17d0ea23728b79c2f20c8419cfcb78a5
Adamsc <sco3.1415@gmail.com>
2015-10-12 15:45:37 +0200

    chatting is now very cool

--- client/templates/application/chat/chatController.js
+++ client/templates/application/chat/chatController.js
@@ -2,8 +2,12 @@ Template.entry.events({
   'keypress input': function(event) {
         if (event.charCode == 13) {
             event.stopPropagation();
-            $('.chat-div').append('<div class="message"><a href="" class="message_profile-pic"></a><a href="" class="message_username">Scott</a><span class="message_timestamp">13:37</span><span class="message_content">' + $('.chat-input-text').val() + '</span></div>');
+            var element = $('.chat-div');
+            element.append('<div class="message"><a href="" class="message_profile-pic"></a><a href="" class="message_username">Scott</a><span class="message_timestamp">13:37</span><span class="message_content">' + $('.chat-input-text').val() + '</span></div>');
             $('.chat-input-text').val("");
+            console.log("scrolltop is " + element.prop("scrollHeight"));
+
+            $('.container-fluid').scrollTop( element.prop("scrollHeight") );
         }
     }
 });
