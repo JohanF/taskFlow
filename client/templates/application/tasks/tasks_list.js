f123fe61543e83cc5cb90a3083f3075804c3866c
JohanF <johan.fredriksson@outlook.com>
2015-10-14 14:43:21 +0200

    Activity sorting

--- client/templates/application/tasks/tasks_list.js
+++ client/templates/application/tasks/tasks_list.js
@@ -6,13 +6,30 @@ if(Meteor.isClient) {
 	});
 
 	Template.tasksList.rendered = function() {
+
+		removedFirst = false;
+
 	    this.$('#taskListDiv').sortable({
+					// start: function(e, ui) {
+					// 	il = ui.item.index();
+					// 		console.log(Blaze.getData(il));
+					// },
 	        stop: function(e, ui) {
+
+					//
+					// console.log("ui ")
+					// console.log(ui);
+
 	          el = ui.item.get(0)
 	          before = ui.item.prev().get(0)
 	          after = ui.item.next().get(0)
-
-	          if(!before) {
+			//   beforebefore = ui.item.prev().prev().get(0);
+			  //
+			//   console.log(el);
+			//   console.log(before);
+			//   console.log(after);
+			//   console.log(beforebefore);
+	          if(!before && !removedFirst) {
 							TaskActivities.insert({
 					      description:'Work stopped', // rename this
 								user: Meteor.userId(),
@@ -38,38 +55,46 @@ if(Meteor.isClient) {
 	            newRank = (Blaze.getData(after).priority +
 	                       Blaze.getData(before).priority)/2
 	            }
-						// if(newFirst){
-						// 	TaskActivities.insert({
-					  //     description:'Work stopped', // rename this
-						// 		user: Meteor.userId(),
-					  //     createdAt: Date.now(),
-						// 		task: Blaze.getData(el)._id
-					  //   });
-						//
-						// 	TaskActivities.insert({
-					  //     description:'Work being performed', // rename this
-						// 		user: Meteor.userId(),
-					  //     createdAt: Date.now(),
-						// 		task: Blaze.getData(newFirst)._id
-					  //   });
-						// }
+
+				if(removedFirst){
+					TaskActivities.insert({
+						description:'Work stopped', // rename this
+						user: Meteor.userId(),
+						createdAt: Date.now(),
+						task: Blaze.getData(el)._id
+					});
+
+					console.log($(this).sortable('toArray', { attribute: 'description' }));
+
+
+					// bfr = ui.item.prev();
+					// newFirst = null;
+					//
+					// while (bfr){
+					// 	newFirst = bfr.get(0);
+					// 	bfr = bfr.prev();
+					// }
+					//
+					// TaskActivities.insert({
+					// 	description:'Work being performed', // rename this
+					// 	user: Meteor.userId(),
+					// 	createdAt: Date.now(),
+					// 	task: Blaze.getData(newFirst)._id
+					// });
+
+					removedFirst = false;
+				}
 
 	          Tasks.update({_id: Blaze.getData(el)._id}, {$set: {priority: newRank}})
 	        }
 	    })
+			// this.$('#taskListDiv').sortable({
 			this.$('#taskListDiv').sortable({
 					start: function(e, ui) {
-						il = ui.item.get(0)
-	          befr = ui.item.prev().get(0)
-	          aftr = ui.item.next().get(0);
-						if(!befr){
-							console.log(Blaze.getData(il));
-							newFrst = aftr;
-							console.log(Blaze.getData(aftr));
-							console.log(Blaze.getData(befr));
+						if(ui.item.index() == 0){
+							removedFirst = true;
 						}
-
-					}
+					},
 				})
 	}
  }
