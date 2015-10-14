a206e51c63862c7321f1a462ac32c740e31ae77a
JohanF <johan.fredriksson@outlook.com>
2015-10-14 10:40:16 +0200

    tasklist activity gen

--- client/templates/application/tasks/tasks_list.js
+++ client/templates/application/tasks/tasks_list.js
@@ -6,9 +6,6 @@ if(Meteor.isClient) {
 	});
 
 	Template.tasksList.rendered = function() {
-
-			newFirst=null;
-
 	    this.$('#taskListDiv').sortable({
 	        stop: function(e, ui) {
 	          el = ui.item.get(0)
@@ -41,35 +38,38 @@ if(Meteor.isClient) {
 	            newRank = (Blaze.getData(after).priority +
 	                       Blaze.getData(before).priority)/2
 	            }
-						if(newFirst){
-							TaskActivities.insert({
-					      description:'Work stopped', // rename this
-								user: Meteor.userId(),
-					      createdAt: Date.now(),
-								task: Blaze.getData(el)._id
-					    });
-
-							TaskActivities.insert({
-					      description:'Work being performed', // rename this
-								user: Meteor.userId(),
-					      createdAt: Date.now(),
-								task: Blaze.getData(newFirst)._id
-					    });
-						}
-
-						newFirst = null;
+						// if(newFirst){
+						// 	TaskActivities.insert({
+					  //     description:'Work stopped', // rename this
+						// 		user: Meteor.userId(),
+					  //     createdAt: Date.now(),
+						// 		task: Blaze.getData(el)._id
+					  //   });
+						//
+						// 	TaskActivities.insert({
+					  //     description:'Work being performed', // rename this
+						// 		user: Meteor.userId(),
+					  //     createdAt: Date.now(),
+						// 		task: Blaze.getData(newFirst)._id
+					  //   });
+						// }
 
 	          Tasks.update({_id: Blaze.getData(el)._id}, {$set: {priority: newRank}})
-	        },
-        start: function(e, ui) {
-	        el = ui.item.get(0)
-	        before = ui.item.prev().get(0)
-					after = ui.item.next().get(0)
-					if(!before){
-						newFirst = after;
-						console.log(Blaze.getData(newFirst)._id);
-					}
-				}
+	        }
 	    })
+			this.$('#taskListDiv').sortable({
+					start: function(e, ui) {
+						il = ui.item.get(0)
+	          befr = ui.item.prev().get(0)
+	          aftr = ui.item.next().get(0);
+						if(!befr){
+							console.log(Blaze.getData(il));
+							newFrst = aftr;
+							console.log(Blaze.getData(aftr));
+							console.log(Blaze.getData(befr));
+						}
+
+					}
+				})
 	}
  }
