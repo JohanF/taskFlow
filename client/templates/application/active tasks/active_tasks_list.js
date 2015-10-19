39c0e1eb555d4cbfd46e1f494a640bc1f86d3514
JohanF <johan.fredriksson@outlook.com>
2015-10-19 16:39:17 +0200

    General pub sub functionality setup, system back to normal

--- client/templates/application/active tasks/active_tasks_list.js
+++ client/templates/application/active tasks/active_tasks_list.js
@@ -1,47 +1,21 @@
-Template.activeTasksList.helpers({ 
+Template.activeTasksList.helpers({
  tasks: function () {
       // @TODO Sort by createDate
 
       var projectTasks = Tasks.find({project:projectData._id}).fetch();
       var userProjectTasks = _.groupBy(projectTasks, 'assignedUsers');
-      
+
       var highPrioTaskObject = [];
 
       for (var key in userProjectTasks) {
             if (userProjectTasks.hasOwnProperty(key)) {
                   var obj = userProjectTasks[key];
-                  highPrioTaskObject.push({name: Meteor.users.findOne({_id:key.toString()}).username, taskName: _.min(obj, function(o){return o.priority}).title});
+                  if(Meteor.users.findOne({_id:key.toString()})){
+                      highPrioTaskObject.push({name: Meteor.users.findOne({_id:key.toString()}).username, taskName: _.min(obj, function(o){return o.priority}).title});
+                  }
             }
       }
 
       return highPrioTaskObject;
-
-      //return highPrioTaskArray;
-
-      // @TODO Implement!  _.groupBy(Tasks.find({project:projectData._id}).fetch(), 'assignedUsers');
-      //return _.min(userProjectTasks, function(temp) { return temp.priority;});        
-      // testing @TODO priority has to be changed to be contexual per user
-      // testing @TODO add active project
-      // _.max(_.map(_.groupBy(Tasks.find({project:projectData._id}).fetch(), 'assignedUsers'), 
-            // function(n,k){ return n; })[0], function(o){return o.priority;});
-
-      // Test 1::
-      // _.map(_.groupBy(Tasks.find({project:"B3MowNggmDxdo5ugL"}).fetch(), 'assignedUsers'), function(obj){_.min(obj,function(o){return o.priority})})
-      
-      // Test 2::
-      //_.each(_.values(_.groupBy(Tasks.find({project:"B3MowNggmDxdo5ugL"}).fetch(), 'assignedUsers')), alert)
-
-      // _.groupBy([], )
-      //_.groupBy(['one', 'two', 'three'], 'length')  => {3: ["one", "two"], 5: ["three"]}
-      /*var taskArray = _.map(userProjectTasks, function(n,k) {return n;});
-      var arrayLength = taskArray.length;
-      var highPrioTaskArray = [];
-
-      for (var i = 0; i < arrayLength; i++) {
-            highPrioTaskArray.push(_.min(taskArray[i], function(o){return o.priority;}));
-      }
-      //var x = Tasks.find({assignedUsers:Meteor.userId(), project:"B3MowNggmDxdo5ugL"});
-      //var tempArray = x.projectTasks || [];
-      */
-    } 
+    }
 });
