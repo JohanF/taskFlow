481b7b3a5d5024028752cde0c1c27b55d0ca31a6
adamscott <carladamscott@gmail.com>
2015-10-22 11:15:45 +0200

    temporary fix to filesystem

--- client/helpers/profileview.js
+++ client/helpers/profileview.js
@@ -9,6 +9,7 @@ Template.profileview.helpers({
    },
    profilePic: function() {
       if(Meteor.user().profile.pic != undefined){
+         console.log("found profilePic")
          return Meteor.user().profile.pic;
       }else{
          return "/img/profile-pic/kungfu-Minion.png";
@@ -22,11 +23,18 @@ Template.imageGallery.helpers({
 });
 Template.profileview.rendered = function() {
     $("#createImageModal").hide();
+
+    //try loading img url's from frontend?
+
     //load img urls
     Meteor.call("getImageFilesInProfileFolder", function(err, data){
       if(err){
-          console.log("error in imageGallery: ")
+          console.log("----- :((( error in imageGallery: ")
           console.log(err);
+      }
+      else{
+         console.log("SUCCESS - got urls! - SUCCESS")
+         console.log(data);
       }
       Session.set("images",data)
    });
--- server/profile.js
+++ server/profile.js
@@ -1,18 +1,29 @@
 
 Meteor.methods({
    getImageFilesInProfileFolder: function() {
+
        var fs = Npm.require('fs');
-       if(process.env.NODE_ENV === "production"){
-         var files = fs.readdirSync('/img/profile-pics/');
+       var base = process.env.PWD;
+       var meteor_root = Npm.require('fs').realpathSync( process.cwd() + '/../' );
+       var application_root = Npm.require('fs').realpathSync( meteor_root + '/../' );
+       var image_folder = meteor_root + '/public/img/profile-pics/';
+
+       if(process.env.NODE_ENV === "development"){
+          path = Npm.require('path');
+          console.log(fs.readdirSync("./assets/app/profile-pics/"));
+         var files = fs.readdirSync("./assets/app/profile-pics/");
+
        }
        else{
-         var files = fs.readdirSync('../../../../../public/img/profile-pics/');
+
+         //var files = fs.readdirSync(base+"meteor/");
+         var files = fs.readdirSync("./assets/app/profile-pics/");
        }
 
-       var cleanedUpFiles = _(files).reject( function(fileName) {
+       /*var cleanedUpFiles = _(files).reject( function(fileName) {
            return fileName.indexOf('.png') < 0;
-       });
-       return cleanedUpFiles;
+       });*/
+       return files;
 
    }
 });
