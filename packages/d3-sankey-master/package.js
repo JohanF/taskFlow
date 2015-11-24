3a8f8bd07d388cef2cccc20bd25148dede2a5229
JohanF <johan.fredriksson@outlook.com>
2015-11-24 13:33:32 +0100

    Added a sankey skeleton

---
+++ packages/d3-sankey-master/package.js
@@ -0,0 +1,13 @@
+Package.describe({
+	name: 'apa:d3',
+    summary: "Sankey D3 Plugin for Meteor"
+});
+
+Package.on_use(function(api, where) {
+    api.use(['d3'], 'client');
+    api.add_files(['sankey.js']);
+
+    if (api.export) {
+        api.export('sankey');
+    }
+});
