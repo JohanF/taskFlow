f6ed630012b2a95b0ebd84050dd1f3dbc99a747f
JohanF <johan.fredriksson@outlook.com>
2015-12-04 11:49:10 +0100

    Updating taskpriority works

--- server/updateTask.js
+++ server/updateTask.js
@@ -1,7 +1,7 @@
 Meteor.methods({
 	updateTaskPriority: function(taskId, uid, priority) {
 		console.log("updating task priority");
-		Tasks.update({_id: taskId, 'assignedUsers.uid': uid}, {$set: {'assignedUsers.priority': priority}})
+		Tasks.update({_id: taskId, 'assignedUsers.uid': uid}, {$set: {'assignedUsers.$.priority': priority}})
 	}
 });
 
