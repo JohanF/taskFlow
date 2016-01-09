19e8378630558e2fb62b2bd2a5fa30b6859d18e1
JohanF <johan.fredriksson@outlook.com>
2016-01-09 18:40:34 +0100

    Displaying users and  name correctly in taskinfomodal

--- client/component/taskModal.js
+++ client/component/taskModal.js
@@ -8,7 +8,17 @@ Template.taskModal.events({
 Template.taskModal.helpers({
  taskActiviy: function () {
       return TaskActivities.find({task:Session.get('selectedTask')}, {sort: {createdAt: -1}});
+    },
+  taskname: function () {
+    return Tasks.findOne({_id:Session.get('selectedTask')}).title;
+  },
+  taskusers: function () {
+    var taskU = "";
+    for(user in Tasks.findOne({_id:Session.get('selectedTask')}).assignedUsers){
+      taskU = taskU + " " + Meteor.users.find({_id:Tasks.findOne({_id:Session.get('selectedTask')}).assignedUsers[user].uid}).fetch()[0].username;
     }
+    return taskU;
+  }
 });
 
 Template.registerHelper('last',
